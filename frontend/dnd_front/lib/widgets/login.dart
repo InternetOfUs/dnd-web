1e5248a1703542a5a98a6c85beb5480042a3848b
William DROZ <william.droz@idiap.ch>
2022-06-20 15:58:46 +0200

    feat(front): Send the username to the norms editor

--- frontend/dnd_front/lib/widgets/login.dart
+++ frontend/dnd_front/lib/widgets/login.dart
@@ -17,7 +17,8 @@ class LoginScreen extends StatelessWidget {
                 'Enter the user id (will be replaced by 0auth2 later...)',
           ),
           onFieldSubmitted: (String value) {
-            Navigator.pushNamed(context, "/norms_editor/");
+            Navigator.pushNamed(context, "/norms_editor/",
+                arguments: {"user": value});
           },
         ),
       ),
