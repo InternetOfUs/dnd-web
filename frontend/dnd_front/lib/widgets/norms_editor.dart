f98669cdaccdda89b784ab8dc7f0fee57905e5ff
William DROZ <william.droz@idiap.ch>
2022-06-20 15:58:03 +0200

    feat(front) WIP widget with user as argument

--- frontend/dnd_front/lib/widgets/norms_editor.dart
+++ frontend/dnd_front/lib/widgets/norms_editor.dart
@@ -10,6 +10,17 @@ class NormsEditor extends StatefulWidget {
 class _NormsEditorState extends State<NormsEditor> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    Map arguments = ModalRoute.of(context)?.settings.arguments as Map;
+
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Norms editor'),
+        ),
+        body: Center(
+          child: Column(children: [
+            Text("You are logged as ${arguments['user']}"),
+            const Text("TODO fill norms", style: TextStyle(color: Colors.red))
+          ]),
+        ));
   }
 }
