191b44f84a68c7937a2212eb7fe4b961fc6edd75
William Droz <william.droz@idiap.ch>
2022-06-22 09:39:50 +0200

    feat(back): Use actix_files directly, add logger

--- backend/dnd_back/src/main.rs
+++ backend/dnd_back/src/main.rs
@@ -1,15 +1,20 @@
-use actix_web::{App, HttpServer};
-use actix_web_static_files::ResourceFiles;
-
-include!(concat!(env!("OUT_DIR"), "/generated.rs"));
+use actix_files as fs;
+use actix_web::{middleware::Logger, App, HttpServer};
+use std::env;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(move || {
-        let generated = generate();
-        App::new().service(ResourceFiles::new("/", generated))
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+    let path = env::current_dir()?;
+    println!("The current directory is {}", path.display());
+    HttpServer::new(|| {
+        App::new().wrap(Logger::new("%a %{User-Agent}i")).service(
+            fs::Files::new("/", "./static")
+                .index_file("index.html")
+                .use_last_modified(true),
+        )
     })
-    .bind("127.0.0.1:8888")?
+    .bind(("127.0.0.1", 8888))?
     .run()
     .await
 }
