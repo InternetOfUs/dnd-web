4919df02d97049e29edc97bff2c8ca8acb20786d
William Droz <william.droz@idiap.ch>
2022-06-22 09:51:10 +0200

    feat(back): remove debug print

--- backend/dnd_back/src/main.rs
+++ backend/dnd_back/src/main.rs
@@ -1,12 +1,9 @@
 use actix_files as fs;
 use actix_web::{middleware::Logger, App, HttpServer};
-use std::env;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
-    let path = env::current_dir()?;
-    println!("The current directory is {}", path.display());
     HttpServer::new(|| {
         App::new().wrap(Logger::new("%a %{User-Agent}i")).service(
             fs::Files::new("/", "./static")
@@ -14,7 +11,7 @@ async fn main() -> std::io::Result<()> {
                 .use_last_modified(true),
         )
     })
-    .bind(("127.0.0.1", 8888))?
+    .bind(("0.0.0.0", 8888))?
     .run()
     .await
 }
