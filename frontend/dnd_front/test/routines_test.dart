a3ebd7e3b57cedabacf85889f6104b3d9528b9b4
William Droz <william.droz@idiap.ch>
2022-06-27 13:55:17 +0200

    feat(front): add tests for all weekdays

--- frontend/dnd_front/test/routines_test.dart
+++ frontend/dnd_front/test/routines_test.dart
@@ -8,4 +8,40 @@ void main() {
 
     expect(day, Weekday.monday);
   });
+  test('2 is Tuesday', () {
+    final routine = Routine(2, "", "", "");
+    final day = routine.toWeekday();
+
+    expect(day, Weekday.tuesday);
+  });
+  test('3 is Wednesday', () {
+    final routine = Routine(3, "", "", "");
+    final day = routine.toWeekday();
+
+    expect(day, Weekday.wednesday);
+  });
+  test('4 is Thursday', () {
+    final routine = Routine(4, "", "", "");
+    final day = routine.toWeekday();
+
+    expect(day, Weekday.thursday);
+  });
+  test('5 is Friday', () {
+    final routine = Routine(5, "", "", "");
+    final day = routine.toWeekday();
+
+    expect(day, Weekday.friday);
+  });
+  test('6 is Saturday', () {
+    final routine = Routine(6, "", "", "");
+    final day = routine.toWeekday();
+
+    expect(day, Weekday.saturday);
+  });
+  test('7 is Sunday', () {
+    final routine = Routine(7, "", "", "");
+    final day = routine.toWeekday();
+
+    expect(day, Weekday.sunday);
+  });
 }
