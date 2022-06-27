32d2e57fd361a45bcab6cc5c3a53a392a85e5fe6
William Droz <william.droz@idiap.ch>
2022-06-27 16:26:07 +0200

    feat(front): create routine entry (WIP)

--- frontend/dnd_front/lib/routines.dart
+++ frontend/dnd_front/lib/routines.dart
@@ -1,13 +1,54 @@
 import 'package:dnd_front/models/login.dart';
 import 'package:dnd_front/models/routines.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 
 class RoutinePage extends StatelessWidget {
   const RoutinePage({Key? key}) : super(key: key);
 
   Widget buildRoutine(BuildContext context, Routine routine) {
-    return ListTile(title: Text(routine.weekdayStr));
+    return Row(
+      children: [
+        SizedBox(width: 90.0, child: Text(routine.weekdayStr)),
+        const SizedBox(width: 60.0, child: Text("From: ")),
+        const SizedBox(
+          width: 100.0,
+          child: TextField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              hintText: '0.00am',
+            ),
+          ),
+        ),
+        const SizedBox(width: 30.0, child: Text("To: ")),
+        const SizedBox(
+          width: 100.0,
+          child: TextField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              hintText: '0.00am',
+            ),
+          ),
+        ),
+        OutlinedButton(
+          onPressed: () {
+            debugPrint('Received click');
+          },
+          child: const Text('add time slot'),
+        ),
+        const SizedBox(
+          width: 200.0,
+          // TODO replace by combo box
+          child: TextField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              hintText: 'where are you usually?',
+            ),
+          ),
+        ),
+      ],
+    );
   }
 
   @override
