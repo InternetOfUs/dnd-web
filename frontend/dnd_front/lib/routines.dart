0430d12e90fb6744db320eef11fc736290d3bdd1
William Droz <william.droz@idiap.ch>
2022-06-27 11:37:21 +0200

    feat(front): show a list of the routines (WIP)

--- frontend/dnd_front/lib/routines.dart
+++ frontend/dnd_front/lib/routines.dart
@@ -1,4 +1,5 @@
 import 'package:dnd_front/models/login.dart';
+import 'package:dnd_front/models/routines.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -11,7 +12,17 @@ class RoutinePage extends StatelessWidget {
         appBar: AppBar(title: const Text('Routines')),
         body: Column(children: [
           Consumer<LoginModel>(
-              builder: (context, loginModel, child) => Text(loginModel.login))
+              builder: (context, loginModel, child) => Text(loginModel.login)),
+          Consumer<RoutinesModel>(
+              builder: (context, routinesModels, child) => ListView.builder(
+                    itemCount: routinesModels.routines.length,
+                    scrollDirection: Axis.vertical,
+                    shrinkWrap: true,
+                    itemBuilder: (context, index) {
+                      final item = routinesModels.routines[index];
+                      return ListTile(title: Text(item.weekdayStr));
+                    },
+                  ))
         ]));
   }
 }
