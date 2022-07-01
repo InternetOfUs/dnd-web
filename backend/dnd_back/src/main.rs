107877722d0a5a3ba044c88491a0cd2aa843fb7e
William Droz <william.droz@idiap.ch>
2022-07-01 09:28:01 +0200

    feat(back): norms structs

--- backend/dnd_back/src/main.rs
+++ backend/dnd_back/src/main.rs
@@ -1,9 +1,42 @@
+use std::fmt::{self};
+
 use actix_files as fs;
-use actix_web::{middleware::Logger, post, App, HttpResponse, HttpServer, Responder};
+use actix_web::{middleware::Logger, post, web, App, HttpResponse, HttpServer, Responder};
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct NormWithUser {
+    userid: String,
+    norm: Norm,
+}
+
+impl fmt::Display for NormWithUser {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{} - {}", self.userid, self.norm)
+    }
+}
+
+#[derive(Deserialize)]
+struct Norm {
+    description: String,
+    whenever: String,
+    thenceforth: String,
+}
+
+impl fmt::Display for Norm {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "\n\tdesc: {}\n\twhen: {}\n\tthen: {}",
+            self.description, self.whenever, self.thenceforth
+        )
+    }
+}
 
 #[post("/add_norms")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
+async fn echo(norm_with_user: web::Json<NormWithUser>) -> impl Responder {
+    println!("received {}", norm_with_user);
+    HttpResponse::Ok().body(norm_with_user.userid.clone())
 }
 
 #[actix_web::main]
